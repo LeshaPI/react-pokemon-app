b542c68cd388361f759680a27777e9688395c3e2
LeshaPI <pialeksej5@gmail.com>
2025-04-16 21:26:14 +0300

    fix: button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,5 @@
 import "./Button.scss"
 
-export default function Button({ children }: {children: string}) {
-    return <button className="button">{ children }</button>
+export default function Button({ children, handler }: {children: string, handler: () => void}) {
+    return <button className="button" onClick={ handler }>{ children }</button>
 }
